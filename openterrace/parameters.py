e4b0881ef1a6c1b22aca2cdd248816996331076a
Jakob Hærvig <jakob.haervig@gmail.com>
2022-01-03 23:03:40 +0100

    Updated parameters.py

--- openterrace/parameters.py
+++ openterrace/parameters.py
@@ -2,27 +2,35 @@ import numpy as np
 
 class Constants():
     def __init__(self):
-        self.dt = 2
-        self.t_end = 3600
+        self.dt = 1
+        self.t_end = 900
         self.h_tank = 2
 
-        self.nx_tank = 7
-        self.nx_particles = 10
+        self.nx_tank = 100
+        self.nx_particles = 200
 
         self.d_p = 2e-2
         self.phi_p = 0.3
 
         self.Tinit = 40
-        self.Tin = 60
+        self.Tfluid = 12
 
         self.u = np.array([0.1])
+        self.bc = 'forced_convection'
 
 class Var():
     class Fluid():
-        def __init__(self, params):
-            self.T = np.full(params.nx_tank, params.Tinit)
-            #self.h = np.zeros(params.nx_tank)
+        def __init__(self, const, prop):
+            self.T = np.full(const.nx_tank, const.Tfluid)
+            self.T_old = np.copy(self.T)
+            self.rho = prop.fluid.rho(self.T)
 
     class Particle():
-        def __init__(self, params):
-            self.T = np.full((params.nx_particles, params.nx_tank), params.Tinit)
+        def __init__(self, const, prop):
+            self.T = np.full((const.nx_particles, const.nx_tank), const.Tinit)
+            self.T_old = np.copy(self.T)
+            self.rho = prop.solid.rho(self.T)
+            self.k = prop.solid.k(self.T)
+            self.cp = prop.solid.cp(self.T)
+            self.bc = const.bc
+            self.h = np.full(const.nx_tank, 200)
