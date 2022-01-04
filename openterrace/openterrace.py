86d33c1df095b1538ea51caa403547832fd040ff
Jakob Hærvig <jakob.haervig@gmail.com>
2022-01-04 10:19:44 +0100

    Added profilers

--- openterrace/openterrace.py
+++ openterrace/openterrace.py
@@ -1,12 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import cProfile, pstats, io
-from pstats import SortKey
 
 import properties
 import parameters
 import solvers
-import plots
+import profilers
 
 if __name__ == '__main__':
     const = parameters.Constants()
@@ -18,6 +16,8 @@ if __name__ == '__main__':
 
     fluid = parameters.Var.Fluid(const, prop)
     particle = parameters.Var.Particle(const, prop)
+
+    profiling = profilers.Profiling()
     
     diff_1d = solvers.Diff1D(const, prop)
     A0 = diff_1d.matrix_assembly_tri(const, particle)
@@ -25,19 +25,13 @@ if __name__ == '__main__':
 
     Tm0 = particle.T_old
 
-    pr = cProfile.Profile()
-    pr.enable()
+    profiling.start()
 
-    for t in range(0,int(const.t_end/const.dt)):
+    for t in range(0, int(const.t_end/const.dt)):
         b = diff_1d.update_b(const, particle, fluid, Tm0)
         Tm0 = diff_1d.solve_tridiagonal(A, b)
 
-    pr.disable()
-    s = io.StringIO()
-    sortby = SortKey.CUMULATIVE
-    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    ps.print_stats()
-    print(s.getvalue())
+    profiling.end()
 
     # diff_1d.analytical(const, particle)
     # plt.plot(diff_1d.r/(const.d_p/2),Tm0,'-s',diff_1d.r/(const.d_p/2),diff_1d.theta*(const.Tinit-const.Tfluid)+const.Tfluid,'-')
