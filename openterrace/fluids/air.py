f78d37620c8b350be2344b24fc7a4ff597e11601
Jakob Hærvig <jakob.haervig@gmail.com>
2022-01-17 08:57:07 +0100

    Updated to return array with input size

--- openterrace/fluids/air.py
+++ openterrace/fluids/air.py
@@ -2,16 +2,16 @@
 
 #Function for density
 def rho(T):
-    return 1.2 #To be updated
+    return 1.2*T**0 #To be updated
 
 #Function for thermal conductivity
 def k(T):
-    return 0.024 #To be updated
+    return 0.024*T**0 #To be updated
 
 #Function for specific heat capacity
 def cp(T):
-    return 1000 #To be updated
+    return 1000*T**0 #To be updated
 
 #Function for dynamic viscosity
 def mu(T):
-    return 1.8e-5 #To be updated
+    return 1.8e-5*T**0 #To be updated
