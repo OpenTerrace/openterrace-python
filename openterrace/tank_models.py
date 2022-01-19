2472a03df12a2c48e0f5f734bc1efb5fd65546d9
Jakob Hærvig <jakob.haervig@gmail.com>
2022-01-19 08:51:45 +0100

    Update tank_models.py

--- openterrace/tank_models.py
+++ openterrace/tank_models.py
@@ -3,8 +3,3 @@ import numpy as np
 class ConvDiff1DExp():
     """This class is used for modelling the tank as a 1D convection-diffusion problem with explicit 1 order temporal discretisation.
         """
-    def __init__(self, const):
-        print("hej")
-
-    def test(self, const):
-        print(const.nx_tank)
