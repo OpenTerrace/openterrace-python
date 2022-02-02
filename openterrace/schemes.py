ec1c3fa676b4b8aacb5b3ea28eee9c072a005883
Jakob Hærvig <jakob.haervig@gmail.com>
2022-02-02 11:58:32 +0100

    Update schemes.py

--- openterrace/schemes.py
+++ openterrace/schemes.py
@@ -1,17 +1,17 @@
 class Convective():
     """Various schemes for discretising the convective term.
-        """
+    """
     def upwind(C, T):
         """First-order accurate upwind scheme.
         """
         return (C[1:-1])*T[:-2] + (-C[1:-1])*T[1:-1]
 
-    # def lax_wendroff(C, T): 
-    #     return T[1:-1] - C/2*(T[2:]-T[:-2]) + C**2.0/2*(T[2:]-2*T[1:-1]+T[:-2])
+        # def lax_wendroff(C, T): 
+        #     return T[1:-1] - C/2*(T[2:]-T[:-2]) + C**2.0/2*(T[2:]-2*T[1:-1]+T[:-2])
 
 class Diffusion():
     """Various schemes for discretising the diffusive term.
-        """
+    """
     def central_difference(C, T):
         """Second-order accurate central diffence scheme.
         """
