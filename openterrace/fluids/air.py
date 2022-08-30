5c3632a3da822ebea20b9f212d437d06b934ae0d
Jakob Hærvig <jakob.haervig@gmail.com>
2022-08-30 15:36:56 +0200

    Updated docstrings

--- openterrace/fluids/air.py
+++ openterrace/fluids/air.py
@@ -1,56 +1,56 @@
-def rho(T):
+def rho(T: float) -> float:
     """Density as function of temperature at 1 atm (valid between 250 K to 1000 K).
 
     Args:
         T (float): Temperature in kelvin
 
     Returns:
-        float: Density in kg/m^3
+        Density in kg/m^3
     """
 
     return 6.99321266968349e-12*T**4 - 2.13949101637962e-08*T**3 + 2.49556828014612e-05*T**2 - 0.0137812349600436*T + 3.57990616714300
 
-def k(T):
+def k(T: float) -> float:
     """Thermal conductivity as function of temperature at 1 atm (valid between 250 K to 1000 K).
 
     Args:
-        T (float): Temperature in kelvin
+        T: Temperature in kelvin
 
     Returns:
-        float: Thermal conductivity in W/(m K)
+        Thermal conductivity in W/(m K)
     """
 
     return -2.74159663865546e-08*T**2 + 9.26081932773110e-05*T + 0.00104196428571427
 
-def cp(T):
+def cp(T: float) -> float:
     """Specific heat capacity as function of temperature at 1 atm (valid between 250 K to 1000 K).
 
     Args:
-        T (float): Temperature in kelvin
+        T: Temperature in kelvin
 
     Returns:
         float: Specific heat capacity in J/(kg K)
     """
     return -3.84827075229561e-07*T**3 + 0.000838847684822935*T**2 -0.364483701712815*T + 1050.66046054049
 
-def mu(T):
+def mu(T: float) -> float:
     """Dynamic viscosity as function of temperature at 1 atm (valid between 250 K to 1000 K).
 
     Args:
-        T (float): Temperature in kelvin
+        T: Temperature in kelvin
 
     Returns:
-        float: Dynamic viscosity in kg/(m s)
+        Dynamic viscosity in kg/(m s)
     """
     return -1.58956582633054e-11*T**2 + 5.46339845938377e-08*T + 3.54058298319323e-06
 
-def Pr(T):
+def Pr(T: float) -> float:
     """Prandtl number as function of temperature at 1 atm (valid between 250 K to 1000 K).
 
     Args:
-        T (float): Temperature in kelvin
+        T): Temperature in kelvin
 
     Returns:
-        float: Prandtl number
+        Prandtl number
     """
     return 9.30380249266224e-16*T**5 - 3.53814400254198e-12*T**4 + 4.52699055330845e-09*T**3 - 2.12904461379820e-06*T**2 + 0.000187441890926772*T + 0.747916944510558
--- openterrace/fluids/water.py
+++ openterrace/fluids/water.py
@@ -10,35 +10,35 @@ def rho(T: float) -> float:
 
     return -0.00365471*T**2 + 1.93017*T + 746.025
 
-def k(T):
+def k(T: float) -> float:
     """Thermal conductivity as function of temperature in K at 1 atm (valid between 273.15 K to 363.15 K).
 
     Args:
-        T (float): Temperature in kelvin
+        T: Temperature in kelvin
 
     Returns:
-        float: Thermal conductivity in W/(m K)
+        Thermal conductivity in W/(m K)
     """
     return -9.29827e-6*T**2 + 0.0071857*T - 0.710696
 
-def cp(T):
+def cp(T: float) -> float:
     """Specific heat capacity as function of temperature in K at 1 atm (valid between 273.15 K to 363.15 K).
         
     Args:
-        T (float): Temperature in kelvin
+        T: Temperature in kelvin
 
     Returns:
-        (float): Specific heat capacity in J/(kg K)
+        Specific heat capacity in J/(kg K)
     """
     return -0.000127063*T**3 + 0.13736*T**2 - 48.6714*T + 9850.69
 
-def mu(T):
+def mu(T: float) -> float:
     """Dynamic viscosity as function of temperature in K at 1 atm (valid between 273.15 K to 363.15 K).
 
     Args:
-        T (float): Temperature in kelvin
+        T: Temperature in kelvin
 
     Returns:
-        float: Dynamic viscosity in kg/(m s)
+        Dynamic viscosity in kg/(m s)
     """
     return -2.80572e-9*T**3 + 2.90283e-6*T**2 - 0.00100532*T + 0.116947
