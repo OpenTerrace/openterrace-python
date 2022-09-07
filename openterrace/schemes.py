913a898d1ab6fc9dee5c0483adf99422d06b8e88
Jakob Hærvig <jakob.haervig@gmail.com>
2022-09-07 15:01:25 +0200

    Update schemes.py

--- openterrace/schemes.py
+++ openterrace/schemes.py
@@ -5,37 +5,37 @@ class Convection:
     """Various schemes for discretising the advection term.
     """
     @nb.njit
-    def upwind_1d(x, Fw=0, Fe=0, Fn=0, Fs=0, y):
+    def upwind_1d(x, F):
         """First-order accurate, unconditionally stable, non-conservative upwind advection scheme.
         """
-        for i in range(1,x.shape[0]-1):
-            for j in range(1,x.shape[1]-1):
-                y[i] = x[i+1]*(-np.minimum(Fs[i],0))\
-                       + x[i-1]*(np.maximum(Fn[i],0))\
-                       + x[i]*(np.minimum(Fs[i],0)-np.maximum(Fn[i],0))
+        y = np.zeros_like(x)
+        for i in range(1, x.shape[0]-1):
+            y[i] = x[i+1]*(-np.minimum(F[0,i],0))\
+                 + x[i-1]*(np.maximum(F[1,i],0))\
+                 + x[i]*(np.minimum(F[0,i],0)-np.maximum(F[1,i],0))
         return y
 
 class Diffusion:
     """Various schemes for discretising the diffusion term.
     """
     @nb.njit
-    def central_difference_1d(x, Dw, De, Dn, Ds, y):
+    def central_difference_1d(x, D):
         """Second-order accurate central diffence scheme.
         """
-        for i in range(1,x.shape[0]-1):
-            for j in range(1,x.shape[1]-1):
-                y[i,j] = x[i,j-1]*Dw[i,j] + x[i,j+1]*De[i,j]\
-                       + x[i+1,j]*Ds[i,j] + x[i-1,j]*Dn[i,j]\
-                       - x[i,j]*(Dw[i,j]+De[i,j]+Ds[i,j]+Dn[i,j])
+        y = np.zeros_like(x)
+        for i in range(1, x.shape[0]-1):
+            y[i] = x[i-1]*D[0,i] + x[i+1]*D[1,i]\
+                 - x[i]*(D[0,i]+D[1,i])
         return y
 
-    @nb.njit
-    def central_difference_2d(x, Dw, De, Dn, Ds, y):
-        """Second-order accurate central diffence scheme.
-        """
-        for i in range(1,x.shape[0]-1):
-            for j in range(1,x.shape[1]-1):
-                y[i,j] = x[i,j-1]*Dw[i,j] + x[i,j+1]*De[i,j]\
-                       + x[i+1,j]*Ds[i,j] + x[i-1,j]*Dn[i,j]\
-                       - x[i,j]*(Dw[i,j]+De[i,j]+Ds[i,j]+Dn[i,j])
-        return y
+    # @nb.njit
+    # def central_difference_2d(x, D0_0, D0_1, D1_0, D1_1):
+    #     """Second-order accurate central diffence scheme.
+    #     """
+    #     y = np.zeros_like(x)
+    #     for i in range(1,x.shape[0]-1):
+    #         for j in range(1,x.shape[1]-1):
+    #             y[i,j] = x[i-1,j]*D0_0[i,j] + x[i+1,j]*D0_1[i,j]\
+    #                    + x[i,j-1]*D1_0[i,j] + x[i,j+1]*D1_1[i,j]\
+    #                    - x[i,j]*(D0_0[i,j]+D0_1[i,j]+D1_0[i,j]+D1_1[i,j])
+    #     return y
