d7b48337cd6477a102060507572a202b3df360b9
Jakob Hærvig <jakob.haervig@gmail.com>
2022-09-26 15:01:33 +0200

    Update generate_fluid_data.py

--- openterrace/generate_fluid_data.py
+++ openterrace/generate_fluid_data.py
@@ -2,27 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import CoolProp.CoolProp as CP
 
-fluid = 'Air'
+fluid = 'Water'
 
-T = np.arange(200,1000,10)
+
+T1 = CP.PropsSI('T','P',101325,'Q',0,fluid)
+T2 = CP.PropsSI('T','P',101325,'Q',1,fluid)
+print(T1,T2)
+
+T = np.arange(200, 1000, 10)
 V = CP.PropsSI('V','P',101325,'T',T,fluid)
 C = CP.PropsSI('C','P',101325,'T',T,fluid)
 L = CP.PropsSI('L','P',101325,'T',T,fluid)
 H = CP.PropsSI('H','P',101325,'T',T,fluid)
 
+# plot data and fit
+plt.plot(H, T, '-ok')
+plt.show()
+
 # calculate polynomial
 z = np.polyfit(H, T, 2)
 f = np.poly1d(z)
 
-# calculate new x's and y's
-H_new = np.linspace(H[0], H[-1], 50)
-T_new = f(H_new)
+# # calculate new x's and y's
+# H_new = np.linspace(H[0], H[-1], 50)
+# T_new = f(H_new)
 
-# print coefficients
-print('coefficients:', z)
-
-# plot data and fit
-plt.plot(H,T,'o', H_new, T_new)
-#plt.xlim([x[0]-1, x[-1] + 1 ])
-plt.show()
+# # print coefficients
+# print('coefficients:', z)
 
