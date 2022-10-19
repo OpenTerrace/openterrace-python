810dddd4c43b84ab44c57ec02a0ed27366e277d9
Jakob Hærvig <jakob.haervig@gmail.com>
2022-10-19 15:02:02 +0200

    Update test_diffusion.py

--- tests/test_diffusion.py
+++ tests/test_diffusion.py
@@ -1,4 +1,4 @@
-from openterrace import OpenTerrace
+import openterrace
 import pytest
 import numpy as np
 from scipy.optimize import brentq
@@ -39,7 +39,7 @@ cp = 1130
 rho = 5150
 k = 1.9
 
-ot = OpenTerrace(t_end=t_end, dt=1e-2, n_bed=n)
+ot = openterrace.OpenTerrace(t_end=t_end, dt=1e-2, n_bed=n)
 ot.bed.define_substance_on_the_fly(cp=cp, rho=rho, k=k)
 ot.bed.select_domain(domain='1d_sphere', D=Lc*2)
 ot.bed.select_schemes(diff='central_difference_1d')
