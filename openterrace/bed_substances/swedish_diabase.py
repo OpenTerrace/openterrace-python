4198efaf09bc2d0677753efaea7e5c41a5af400c
Jakob Hærvig <jakob.haervig@gmail.com>
2022-10-19 21:50:21 +0200

    Update

--- openterrace/bed_substances/swedish_diabase.py
+++ openterrace/bed_substances/swedish_diabase.py
@@ -1,32 +1,58 @@
-def fcn_rho(T):
-    """Dummy function for constant density.
+def h(T: float) -> float:
+    """Mass specific enthalpy as function of temperature at 1 atm (fit assumes constant cp).
 
     Args:
-        T (float): Temperature in kelvin
+        T (float): Temperature in K
 
     Returns:
-        float: Density in kg/m^3
+        Specific enthalpy in J/kg
     """
-    return 3007*T**0
+    return 1272*T
 
-def fcn_k(T):
-    """Dummy function for constant thermal conductivity.
+def T(h: float, p:float=None) -> float:
+    """Temperature as function of mass specific enthalpy at 1 atm (fit assumes constant cp).
 
     Args:
-        T (float): Temperature in kelvin
+        h (float): Specific enthalpy in J/kg
+        p (float): Pressure in Pa
+
+    Returns:
+        Temperature in kelvin
+    """
+    return 1/1272*h
+
+def rho(h: float, p:float=None) -> float:
+    """Density as function of mass specific entahlpy at 1 atm (fit assumes constant density).
+
+    Args:
+        h (float): Specific enthalpy in J/kg
+        p (float): Pressure in Pa
+
+    Returns:
+        Density in kg/m^3
+    """
+    return 3007*h**0
+
+def k(h: float, p:float=None) -> float:
+    """Thermal conductivity as function of mass specific enthalpy at 1 atm (fit assumes constant thermal conductivity).
+
+    Args:
+        h (float): Specific enthalpy in J/kg
+        p (float): Pressure in Pa
 
     Returns:
         float: Thermal conductivity in W/(m K)
     """
-    return 1.75*T**0
+    return 1.75*h**0
 
-def fcn_cp(T):
-    """Dummy function for constant specific heat capacity.
+def cp(h: float, p:float=None) -> float:
+    """Specific heat capacity as function of mass specific enthalpy at 1 atm (fit assumes constant specific heat capacity).
 
     Args:
-        T (float): Temperature in kelvin
+        h (float): Specific enthalpy in J/kg
+        p (float): Pressure in Pa
 
     Returns:
         float: Specific heat capacity in J/(kg K)
     """
-    return 1272*T**0
+    return 1272*h**0
