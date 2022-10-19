db04db1ed036b6e922a1c43754e14eeded443a69
Jakob Hærvig <jakob.haervig@gmail.com>
2022-10-19 12:59:13 +0200

    Update test_diffusion.py

--- tests/test_diffusion.py
+++ tests/test_diffusion.py
@@ -1,9 +1,11 @@
+from openterrace import OpenTerrace
 import pytest
 import numpy as np
 from scipy.optimize import brentq
-
-def test_sphere():
-    def theta_fcn(Bi=None, Fo=None, r_r0=None, n_terms=5):
+import matplotlib.pyplot as plt
+ 
+def test_sphere(Bi:float, Fo:float):
+    def theta_fcn(Bi:float, Fo:float, r_r0:float, n_terms:int=20):
         def lambda_fcn(Bi, i):
             left = np.pi*(i) + 1e-12
             right = np.pi*(i+1) - 1e-12
@@ -20,16 +22,45 @@ def test_sphere():
                 theta += 4*(np.sin(lambda_i)-lambda_i*np.cos(lambda_i)) / (2*lambda_i - np.sin(2*lambda_i)) * np.exp(-lambda_i**2*Fo) * np.sin(lambda_i*r_r0)/(lambda_i*r_r0)
         return theta
 
-    h = 1200
-    Lc = 0.025
-    k = 0.627
-    Bi = h*Lc/k
-    Fo = 0.2085
-    r_r0 = 0
+    theta_arr = []
+    r_r0_arr = np.linspace(0,1,n)
+    for r_r0 in r_r0_arr:
+        theta = theta_fcn(Bi, Fo, r_r0)
+        theta_arr.append(theta)
+    return r_r0_arr, theta_arr
 
-    theta = theta_fcn(Bi, Fo, r_r0)
-    Ti = 5
-    Tinf = 95
-    T = theta*(Ti-Tinf)+Tinf
-    print(theta, T)
-    assert 1 == 1
+n = 50
+t_end = 240
+Lc = 0.025
+T_init = 0
+T_inf = 100
+h = 100000
+cp = 1130
+rho = 5150
+k = 1.9
+
+ot = OpenTerrace(t_end=t_end, dt=1e-2, n_bed=n)
+ot.bed.define_substance_on_the_fly(cp=cp, rho=rho, k=k)
+ot.bed.select_domain(domain='1d_sphere', D=Lc*2)
+ot.bed.select_schemes(diff='central_difference_1d')
+ot.bed.initialise(T=T_init)
+ot.bed.define_bc(bc_type='dirichlet', parameter='T', position=np.s_[:,-1], value=T_inf)
+ot.bed.define_bc(bc_type='neumann', parameter='T', position=np.s_[:,0])
+ot.run_simulation()
+
+Bi = h*Lc/k
+Fo = (k/(rho*cp))*t_end/Lc**2
+
+r_r0_ana, theta_ana = test_sphere(Bi, Fo)
+r_r0_num, theta_num = ot.bed.domain.node_pos/(ot.bed.domain.node_pos[-1]-ot.bed.domain.node_pos[0]), (ot.bed.T[0,:]-T_inf)/(T_init-T_inf)
+
+plt.plot(r_r0_num, theta_num,'s', label='OpenTerrace', color = '#4cae4f')
+plt.plot(r_r0_ana, theta_ana,'k', label='Analytical')
+plt.xlim([0,1])
+plt.ylim([-0.1,1.1])
+plt.grid()
+plt.legend([r"OpenTerrace ($Bi=\infty$, "+r"$Fo=$"+f"{Fo:.2e}", "Analytical"], loc ="lower left")
+plt.xlabel(r'Radial position, $r^* = r/r_0$')
+plt.ylabel(r'Temperature, $\theta = (T-T_\infty)/(T_{init}-T_\infty)$')
+plt.savefig('docs/_figures/test_sphere_0.svg', bbox_inches='tight')
+plt.show()
