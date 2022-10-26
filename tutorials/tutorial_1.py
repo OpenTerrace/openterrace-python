51cafde5553a20ff154c576585ce5f86b6d37d8f
Jakob Hærvig <jakob.haervig@gmail.com>
2022-10-26 20:39:50 +0200

    Create tutorial_1.py

---
+++ tutorials/tutorial_1.py
@@ -0,0 +1,28 @@
+import openterrace
+import matplotlib.pyplot as plt
+import numpy as np
+
+ot = openterrace.GlobalParameters(t_end=3600*10, dt=0.1, n_fluid=50, n_bed=10)
+
+ot.fluid.define_substance_on_the_fly(cp=1000, rho=1.2, k=0.024)
+ot.fluid.select_domain(domain='1d_cylinder', D=1, H=5)
+ot.fluid.add_porosity(phi=0.4)
+ot.fluid.select_schemes(diff='central_difference_1d', conv='upwind_1d')
+ot.fluid.initialise(T=273.15+20, mdot=0.1)
+ot.fluid.define_bc(bc_type='dirichlet', parameter='T', position=(slice(None, None, None), 0), value=273.15+50)
+ot.fluid.define_bc(bc_type='neumann', parameter='T', position=(slice(None, None, None), -1))
+
+ot.bed.define_substance_on_the_fly(cp=1130, rho=5150, k=1.9)
+ot.bed.select_domain(domain='1d_sphere', R=0.005)
+ot.bed.select_schemes(diff='central_difference_1d')
+ot.bed.initialise(T=273.15+20)
+ot.bed.define_bc(bc_type='neumann', parameter='T', position=(slice(None, None, None), 0))
+ot.bed.define_bc(bc_type='neumann', parameter='T', position=(slice(None, None, None), -1))
+
+ot.define_coupling(h_coeff='constant', h_value=5)
+ot.run_simulation()
+
+plt.plot(ot.fluid.domain.node_pos,np.mean(ot.bed.T,1))
+plt.plot(ot.fluid.domain.node_pos,ot.fluid.T[0])
+plt.grid()
+plt.show()
