40401c7b787abfd1568334218d22f4add36c7827
Jakob Hærvig <jakob.haervig@gmail.com>
2022-11-10 10:27:33 +0100

    Update

--- openterrace/openterrace.py
+++ openterrace/openterrace.py
@@ -6,11 +6,13 @@ from . import diffusion_schemes
 from . import convection_schemes
 
 # Import common Python modules
-import sys
+import __main__
 import tqdm
 import numpy as np
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation as anim
 
 class GlobalParameters:
     """OpenTerrace class."""
@@ -34,6 +36,7 @@ class GlobalParameters:
         self.list_postprocess = []
         self.save_data_flag = np.full(int(np.floor(t_end/dt))+1, False)
         self.animate_data_flag = False
+        self.filename = __main__.__file__.split('/')[-1].rsplit( ".", 1)[0]
 
     class Phase:
         """Main class to define either the fluid or bed phase."""
@@ -46,8 +49,8 @@ class GlobalParameters:
             self._type = _type
             self.phi = 1
 
-        def define_substance_on_the_fly(self, cp:float=None, rho:float=None, k:float=None, mu:float=None):
-            """Defines a new substance on-the-fly. This is useful for defining a substance for testing purposes with temperature independent properties.
+        def select_substance_on_the_fly(self, cp:float=None, rho:float=None, k:float=None, mu:float=None):
+            """Defines and selects a new substance on-the-fly. This is useful for defining a substance for testing purposes with temperature independent properties.
 
             Args:
                 cp (float): Specific heat capacity in J/(kg K)
@@ -77,7 +80,7 @@ class GlobalParameters:
                 raise Exception(substance+" specified as "+self._type+" substance. Valid "+self._type+" substances are:", globals()[self.options].__all__)
             self.fcns = getattr(globals()[self.options], substance)
 
-        def select_domain(self, domain:str=None, **kwargs):
+        def select_domain_shape(self, domain:str=None, **kwargs):
             """Select domain shape and initialise constants."""
             kwargs['n'] = self.n
             if not domain:
@@ -92,7 +95,7 @@ class GlobalParameters:
             self.domain.A = self.domain.A(kwargs)
             self.domain.V = self.domain.V(kwargs)
 
-        def add_porosity(self, phi=1):
+        def select_porosity(self, phi=1):
             self.domain.V = self.domain.V*phi
             self.phi = phi
 
@@ -114,7 +117,7 @@ class GlobalParameters:
             else:
                 self.conv = None
 
-        def initialise(self, T=None, mdot=None):
+        def select_initial_conditions(self, T=None, mdot=None):
             """Initialises temperature and massflow fields"""
             if T is not None:
                 self.T = np.tile(T,(np.append(self.n2,self.domain.shape)))
@@ -129,7 +132,32 @@ class GlobalParameters:
             self.F = np.zeros(((2,)+(self.T.shape)))
             self.S = np.zeros(self.T.shape)
 
-        def update_properties(self):
+        def select_bc(self, bc_type=None, parameter=None, position=None, value=None):
+            """Specify boundary condition type"""
+            valid_bc_types = ['neumann','dirichlet']
+            if bc_type not in valid_bc_types:
+                raise Exception("bc_type \'"+bc_type+"\' specified. Valid options for bc_type are:", valid_bc_types)
+            valid_parameters = ['T','mdot']
+            if parameter not in valid_parameters:
+                raise Exception("parameter \'"+parameter+"\' specified. Valid options for parameter are:", valid_parameters)
+            if not position:
+                raise Exception("Keyword 'position' not specified.")
+            if value is None and bc_type=='dirichlet':
+                raise Exception("Keyword 'value' is needed for dirichlet type bc.")
+            self.bcs.append({'type': bc_type, 'parameter': parameter, 'position': position, 'value': value})
+
+        def select_source_term(self, **kwargs):
+            valid_source_types = ['thermal_resistance']
+            if kwargs['source_type'] not in valid_source_types:
+                raise Exception("source_type \'"+kwargs['source_type']+"\' specified. Valid options for source_type are:", valid_source_types)
+            if kwargs['source_type'] == 'thermal_resistance':
+                required = ['R','T_inf', 'position']
+                for var in required:
+                    if not var in kwargs:
+                        raise Exception("Keyword \'"+var+"\' not specified for source of type \'"+kwargs['source_type']+"\'")
+            self.sources.append(kwargs)
+
+        def _update_properties(self):
             """Updates properties based on specific enthalpy"""
             self.T = self.fcns.T(self.h)
             self.rho = self.fcns.rho(self.h)
@@ -144,56 +172,31 @@ class GlobalParameters:
                 self.F[0,:,:] = self.mdot*self.cp
                 self.F[1,:,:] = self.mdot*self.cp
 
-        def define_bc(self, bc_type=None, parameter=None, position=None, value=None):
-            """Specify boundary condition type"""
-            valid_bc_types = ['neumann','dirichlet']
-            if bc_type not in valid_bc_types:
-                raise Exception("bc_type \'"+bc_type+"\' specified. Valid options for bc_type are:", valid_bc_types)
-            valid_parameters = ['T','mdot']
-            if parameter not in valid_parameters:
-                raise Exception("parameter \'"+parameter+"\' specified. Valid options for parameter are:", valid_parameters)
-            if not position:
-                raise Exception("Keyword 'position' not specified.")
-            if value is None and bc_type=='dirichlet':
-                raise Exception("Keyword 'value' is needed for dirichlet type bc.")
-            self.bcs.append({'type': bc_type, 'parameter': parameter, 'position': position, 'value': value})
-
-        def define_source_term(self, **kwargs):
-            valid_source_types = ['thermal_resistance']
-            if kwargs['source_type'] not in valid_source_types:
-                raise Exception("source_type \'"+kwargs['source_type']+"\' specified. Valid options for source_type are:", valid_source_types)
-            if kwargs['source_type'] == 'thermal_resistance':
-                required = ['R','T_inf', 'position']
-                for var in required:
-                    if not var in kwargs:
-                        raise Exception("Keyword \'"+var+"\' not specified for source of type \'"+kwargs['source_type']+"\'")
-            self.sources.append(kwargs)
-
-        def update_boundary_nodes(self, dt):
+        def _update_boundary_nodes(self, dt):
             """Update boundary nodes"""
             for bc in self.bcs:
                 if bc['type'] == 'dirichlet':
                     self.h[bc['position']] = self.fcns.h(bc['value'])
                 if bc['type'] == 'neumann':
                     if bc['position'] == np.s_[:,0]:
-                        self.h[bc['position']] = self.h[bc['position']] + (2*self.T[:,1]*self.D[0,:,1] - 2*self.T[:,0]*self.D[1,:,0]) / (self.rho[:,0]*self.domain.V[0])*dt
+                        self.h[bc['position']] = self.h[bc['position']] + (2*self.T[:,1]*self.D[0,:,1] - 2*self.T[:,0]*self.D[1,:,0] - self.F[0,:,1]*self.T[:,1] + self.F[1,:,0]*self.T[:,0]) / (self.rho[:,0]*self.domain.V[0])*dt
                     if bc['position'] == np.s_[:,-1]:
-                        self.h[bc['position']] = self.h[bc['position']] + (2*self.T[:,-2]*self.D[1,:,-2] - 2*self.T[:,-1]*self.D[0,:,-1]) / (self.rho[:,-1]*self.domain.V[-1])*dt
+                        self.h[bc['position']] = self.h[bc['position']] + (2*self.T[:,-2]*self.D[1,:,-2] - 2*self.T[:,-1]*self.D[0,:,-1] + self.F[1,:,-2]*self.T[:,-2] - self.F[0,:,-1]*self.T[:,-1]) / (self.rho[:,-1]*self.domain.V[-1])*dt
 
-        def add_sources(self, dt):
+        def _update_source(self, dt):
             for source in self.sources:
                 if source['source_type'] == 'thermal_resistance':
                     self.h[source['position']] = self.h[source['position']] + (2/source['R'] * (source['T_inf']-self.T[source['position']])) / (self.rho[source['position']]*self.domain.V[source['position'][1]])*dt
 
-        def solve_equations(self, dt):
+        def _solve_equations(self, dt):
             if self.diff is not None:
                 self.h = self.h + self.diff(self.T, self.D)/(self.rho*self.domain.V)*dt
             if self.conv is not None:
                 self.h = self.h + self.conv(self.T, self.F)/(self.rho*self.domain.V)*dt
             if self.sources is not None:
-                self.add_sources(dt)
+                self._update_source(dt)
 
-    def define_coupling(self, h_coeff='constant', h_value=None):
+    def select_coupling(self, h_coeff='constant', h_value=None):
         self.coupling = True
         valid_h_coeff = ['constant']
         if h_coeff not in valid_h_coeff:
@@ -201,7 +204,7 @@ class GlobalParameters:
         if h_coeff == 'constant':
             self.h_value = h_value
 
-    def couple(self):
+    def _couple(self):
         Q = self.h_value*self.bed.domain.A[1,-1]*(self.fluid.T[0]-self.bed.T[:,-1])*self.dt
         self.bed.h[:,-1] = self.bed.h[:,-1] + Q/(self.bed.rho[:,-1]*self.bed.domain.V[-1])
         self.fluid.h[0] = self.fluid.h[0] - (1-self.fluid.phi)*(self.fluid.domain.V/self.fluid.phi) / np.sum(self.bed.domain.V) * Q/(self.fluid.rho*self.fluid.domain.V)
@@ -213,52 +216,55 @@ class GlobalParameters:
         self.save_int = save_int
         self.saved_bed_data = np.zeros((len(np.arange(self.t_start,self.t_end,save_int*self.dt)), self.fluid.n, self.bed.n))
         self.saved_fluid_data = np.zeros((len(np.arange(self.t_start,self.t_end,save_int*self.dt)), self.fluid.n))
+        self.saved_time_data = np.zeros(len(np.arange(self.t_start,self.t_end,save_int*self.dt)))
         self.save_data_flag[range(0, int(np.floor(self.t_end/self.dt))+1, save_int)] = True
         self.animate_data_flag = True
 
-    def create_animation(self, data):
+    def _create_animation(self, phase=None, xdata=None, ydata=None):
         fig, ax = plt.subplots()
-        xdata, ydata = [], []
-        ln, = ax.plot([], [], '-k')
+        fig.tight_layout(pad=2)
+        ymin = np.min(ydata)-273.15
+        ymax = np.max(ydata)-273.15
+        xmin = np.min(xdata)
+        xmax = np.max(xdata)
 
-        def init():
-            ax.set_xlim(0, np.max(self.bed.domain.node_pos))
-            ax.set_ylim(np.min(data)-273.15, np.max(data)-273.15)
+        def _update(frame):
+            x = xdata
+            y = ydata[frame]
+            ax.clear()
+            ax.set_xlim(xmin, xmax)
+            ax.set_ylim(ymin, ymax)
             ax.set_xlabel('Radial position (m)')
             ax.set_ylabel('Temperature (C)')
-            return ln,
+            ax.grid()
+            ax.plot(x, y.T-273.15, color = '#4cae4f')
+            ax.text(.05, .95, 'Simulated with OpenTerrace', ha='left', va='top', transform=ax.transAxes, color = '#4cae4f',
+                bbox=dict(facecolor='white',boxstyle="square,pad=0.5", alpha=0.5))
+            ax.set_title('Time: ' + str(np.round(self.saved_time_data[frame], decimals=2)) + ' s')
 
-        def update(frame):
-            xdata = self.bed.domain.node_pos
-            ydata = data[frame]
-            ln.set_data(xdata, ydata-273.15)
-            return ln,
-
-        ani = FuncAnimation(fig, update, frames=np.arange(int(np.floor(self.t_end/(self.dt*self.save_int)))),
-                            init_func=init, blit=True)
-        plt.plot()
-        plt.grid()
-        plt.show()
+        ani = anim.FuncAnimation(fig, _update, frames=np.arange(int(np.floor(self.t_end/(self.dt*self.save_int)))))
+        ani.save(filename='animation_'+self.filename+'_'+phase+'.gif', writer=anim.PillowWriter(fps=30),progress_callback=lambda i, n: print(f'Saving frame {i} of {n}'))
 
     def run_simulation(self):
         """This is the function full of magic."""
         i = 0
         for t in tqdm.tqdm(np.arange(self.t_start, self.t_end, self.dt)):
             if hasattr(self.bed, 'T'):
-                self.bed.update_boundary_nodes(self.dt)
-                self.bed.solve_equations(self.dt)
-                self.bed.update_properties()
+                self.bed._update_boundary_nodes(self.dt)
+                self.bed._solve_equations(self.dt)
+                self.bed._update_properties()
             
             if hasattr(self.fluid, 'T'):
-                self.fluid.update_boundary_nodes(self.dt)
-                self.fluid.solve_equations(self.dt)
-                self.fluid.update_properties()
+                self.fluid._update_boundary_nodes(self.dt)
+                self.fluid._solve_equations(self.dt)
+                self.fluid._update_properties()
 
             if self.coupling:
-                self.couple()
+                self._couple()
 
             if self.save_data_flag[i]:
                 if np.mod(i, self.save_int) == 0:
+                    self.saved_time_data[int(i/self.save_int)] = t
                     if hasattr(self.bed, 'T'):
                         self.saved_bed_data[int(i/self.save_int),:,:] = self.bed.T
                     if hasattr(self.fluid, 'T'):
@@ -267,6 +273,6 @@ class GlobalParameters:
 
         if self.animate_data_flag:
             if hasattr(self.bed, 'T'):
-                self.create_animation(self.saved_bed_data)
+                self._create_animation(phase='bed', xdata=self.bed.domain.node_pos, ydata=self.saved_bed_data)
             if hasattr(self.fluid, 'T'):
-                self.create_animation(self.saved_fluid_data)
+                self._create_animation(phase='fluid', xdata=self.fluid.domain.node_pos, ydata=self.saved_fluid_data)
--- tutorials/tutorial_3.py
+++ tutorials/tutorial_3.py
@@ -1,14 +1,14 @@
 import openterrace
 import numpy as np
 
-ot = openterrace.GlobalParameters(t_end=5*60, dt=0.01, n_bed=50)
+ot = openterrace.GlobalParameters(t_end=30*60, dt=0.01, n_bed=100)
 
 D = 0.02
 T_init = 40+273.15
 T_room = 80+273.15
 h = 200
 
-ot.bed.select_substance('swedish_diabase')
+ot.bed.select_substance('ATS58')
 ot.bed.select_domain_shape(domain='sphere_1d', R=D/2)
 ot.bed.select_schemes(diff='central_difference_1d')
 ot.bed.select_initial_conditions(T=T_init)
@@ -16,5 +16,5 @@ ot.bed.select_bc(bc_type='neumann', parameter='T', position=(slice(None, None, N
 ot.bed.select_bc(bc_type='neumann', parameter='T', position=(slice(None, None, None), -1))
 ot.bed.select_source_term(source_type='thermal_resistance', R=1/(h*4*np.pi*(D/2)**2), T_inf=T_room, position=(slice(None, None, None), -1))
 
-ot.animate(save_int=100, animate_data_flag=True)
+ot.animate(save_int=500, animate_data_flag=True)
 ot.run_simulation()
