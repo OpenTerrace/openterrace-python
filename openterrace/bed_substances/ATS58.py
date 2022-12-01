3ac357e2f7da0c429a8661039d411590025eeb0f
Jakob Hærvig <jakob.haervig@gmail.com>
2022-12-01 16:05:11 +0100

    Update ATS58.py

--- openterrace/bed_substances/ATS58.py
+++ openterrace/bed_substances/ATS58.py
@@ -26,7 +26,7 @@ _cp = 3000 #Specific heat capacity
 _rho_l = 1280 #Liquid density
 _rho_s = _rho_l #Solid density
 _h_s = _T_s*_cp #Mass specific enthalpy at point of solidification
-_h_l = _T_s*_cp+_h_f #Mass specific enthalpy after phase shift
+_h_l = _h_s+_h_f #Mass specific enthalpy after phase shift
 
 def h(T: float) -> float:
     """Mass specific enthalpy as function of temperature at 1 atm (fit assumes piecewice constant cp with phase change).
@@ -37,8 +37,7 @@ def h(T: float) -> float:
     Returns:
         Specific enthalpy in J/kg
     """
-    return np.piecewise(T, [T <= _T_s, (T > _T_s) & (T < _T_l), T > _T_l], [lambda T: _cp*T, lambda T: _cp*_T_s + (T-_T_s)/(_T_l-_T_s)*_h_f, lambda T: _cp*_T_s + _h_f + _cp*(T-_T_l)])
-
+    return np.piecewise(T, [T <= _T_s, (T > _T_s) & (T <= _T_l), T > _T_l], [lambda T: _cp*T, lambda T: _h_s + (T-_T_s)/(_T_l-_T_s)*_h_f, lambda T: _h_l + _cp*(T-_T_l)])
 
 def T(h: float, p:float=None) -> float:
     """Temperature as function of mass specific enthalpy at 1 atm (fit assumes piecewice constant cp with phase change).
@@ -50,7 +49,7 @@ def T(h: float, p:float=None) -> float:
     Returns:
         Temperature in kelvin
     """
-    return np.piecewise(h, [h <= _h_s, (h > _h_s) & (h <= _h_l), h > _h_l], [lambda h: 1/_cp*h, lambda h: 1/_cp*_h_s + (_T_l-_T_s)*(h-_h_s)/(_h_l-_h_s), lambda h: 1/_cp*_h_s + (_T_l-_T_s) + 1/_cp*(h-_h_l)])
+    return np.piecewise(h, [h <= _h_s, (h > _h_s) & (h <= _h_l), h > _h_l], [lambda h: 1/_cp*h, lambda h: _T_s + (_T_l-_T_s)*(h-_h_s)/(_h_l-_h_s), lambda h: _T_l + 1/_cp*(h-_h_l)])
 
 def rho(h):
     """Density as function of mass specific entahlpy at 1 atm (fit assumes constant density).
@@ -72,7 +71,7 @@ def k(T):
     Returns:
         float: Thermal conductivity in W/(m K)
     """
-    return np.piecewise(T, [T <= _T_s, (T > _T_s) & (T <= _T_l), T > _T_l], [_k_s, lambda T: (_k_l-_k_s)/(_T_l-_T_s)*(T-_T_s)+_k_s, _k_l])
+    return np.piecewise(h, [h <= _h_s, (h > _h_s) & (h <= _h_l), h > _h_l], [_k_s, lambda h: _k_s + (_k_l-_k_s)/(_h_l-_h_s)*(h-_h_s), _k_l])
 
 def cp(h: float, p:float=None) -> float:
     """Specific heat capacity as function of mass specific enthalpy at 1 atm (fit assumes piecewice constant cp with phase change).
